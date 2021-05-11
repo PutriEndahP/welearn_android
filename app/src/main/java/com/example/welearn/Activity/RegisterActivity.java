b7399ae05ac7ce3673ef1660e924700cee885b99
Putri Endah P <putriendah11@gmail.com>
2021-05-11 23:24:35 +0700

    register java

--- app/src/main/java/com/example/welearn/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/welearn/Activity/RegisterActivity.java
@@ -1,16 +1,31 @@
 package com.example.welearn.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.welearn.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView title;
+    CardView cardView, cardView1, cardView2, cardView3, cardView4, cardView5, cardView6;
+    EditText nama, username, email, password, konfirm_pass, tgl_lahir;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        title = (TextView)findViewById(R.id.judul);
+        cardView = (CardView)findViewById(R.id.card_nama);
+        cardView1 = (CardView)findViewById(R.id.card_username);
+        cardView2 = (CardView)findViewById(R.id.card_email);
+        cardView3 = (CardView)findViewById(R.id.card_password);
+        cardView4 = (CardView)findViewById(R.id.card_konfirmasi_password);
+        cardView5 = (CardView)findViewById(R.id.card_tgl_lahir);
+        cardView6 = (CardView)findViewById(R.id.btn_register);
     }
 }

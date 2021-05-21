7e420adf3845200398b4e61e98b880316c3dfdb5
Putri Endah P <putriendah11@gmail.com>
2021-05-21 07:51:41 +0700

    layout soal huruf

---
+++ app/src/main/java/com/example/welearn/Activity/SoalHurufActivity.java
@@ -0,0 +1,14 @@
+package com.example.welearn.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SoalHurufActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soal_huruf);
+    }
+}

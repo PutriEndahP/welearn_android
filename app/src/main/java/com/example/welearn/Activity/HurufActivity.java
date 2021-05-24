525060e1efd4b8fe5b9ca3d2e72a5a7e08710efa
Yasinta Yusniawati <yasintayusniawati104@gmail.com>
2021-05-24 15:16:42 +0700

    soal coba

---
+++ app/src/main/java/com/example/welearn/Activity/HurufActivity.java
@@ -0,0 +1,14 @@
+package com.example.welearn.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HurufActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huruf);
+    }
+}

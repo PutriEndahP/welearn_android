94fe641db3110f3a11bed40becbdc32c5bbe858b
Putri Endah P <putriendah11@gmail.com>
2021-05-27 21:05:47 +0700

    add lv3

---
+++ app/src/main/java/com/example/welearn/Activity/HurufLv3Activity.java
@@ -0,0 +1,14 @@
+package com.example.welearn.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HurufLv3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_huruf_lv3);
+    }
+}

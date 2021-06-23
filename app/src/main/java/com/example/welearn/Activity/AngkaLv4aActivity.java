1a491253fb51c8c52805a52fe08895427977ae50
Arini <arinipuspitasari6@gmail.com>
2021-06-23 19:19:48 +0700

    tambahan submit angka

--- app/src/main/java/com/example/welearn/Activity/AngkaLv4aActivity.java
+++ app/src/main/java/com/example/welearn/Activity/AngkaLv4aActivity.java
@@ -3,15 +3,38 @@ package com.example.welearn.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.welearn.R;
+import com.example.welearn.Response.Api.ResponsePredict;
+import com.example.welearn.Retrofit.ApiClientWelearn;
+import com.example.welearn.Retrofit.ServerWelearn;
 import com.example.welearn.Retrofit.TokenManager;
 import com.williamww.silkysignature.views.SignaturePad;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AngkaLv4aActivity extends AppCompatActivity {
 
@@ -38,8 +61,8 @@ public class AngkaLv4aActivity extends AppCompatActivity {
         card_soalangka = (CardView) findViewById(R.id.card_soalangka);
         card_reset = (CardView) findViewById(R.id.card_reset);
         card_submit = (CardView) findViewById(R.id.card_submit);
-        card_soalangka1 = (CardView)findViewById(R.id.card_soalangka1);
-        card_soalangka2 = (CardView)findViewById(R.id.card_soalangka2);
+        card_soalangka1 = (CardView) findViewById(R.id.card_soalangka1);
+        card_soalangka2 = (CardView) findViewById(R.id.card_soalangka2);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,7 +70,170 @@ public class AngkaLv4aActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-        padjawabangka1 = (SignaturePad) findViewById(R.id.padjawabangka1);
-        padjawabangka2 = (SignaturePad) findViewById(R.id.padjawabangka2);
+
+        padjawabangka1 = (SignaturePad) findViewById(R.id.huruf_pad);
+        padjawabangka1.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+
+            }
+
+            @Override
+            public void onSigned() {
+                card_submit.setEnabled(true);
+                card_reset.setEnabled(true);
+            }
+
+            @Override
+            public void onClear() {
+                card_submit.setEnabled(false);
+                card_reset.setEnabled(false);
+            }
+        });
+        padjawabangka2 = (SignaturePad) findViewById(R.id.huruf_pad2);
+        padjawabangka2.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+
+            }
+
+            @Override
+            public void onSigned() {
+                card_submit.setEnabled(true);
+                card_reset.setEnabled(true);
+            }
+
+            @Override
+            public void onClear() {
+                card_submit.setEnabled(false);
+                card_reset.setEnabled(false);
+            }
+        });
+        {
+
+            card_reset.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    padjawabangka1.clear();
+                    padjawabangka2.clear();
+                }
+            });
+
+            card_submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    File file = new File(getApplicationContext().getCacheDir(), "huruf");
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Bitmap hurufBitmap = padjawabangka1.getSignatureBitmap();
+                    Bitmap hurufBitmap2 = padjawabangka2.getSignatureBitmap();
+
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    ByteArrayOutputStream byteArrayOutputStream2 = new ByteArrayOutputStream();
+
+                    hurufBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                    byte[] bitmap_data = byteArrayOutputStream.toByteArray();
+                    hurufBitmap2.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream2);
+                    byte[] bitmap_data2 = byteArrayOutputStream2.toByteArray();
+
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream(file);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        fos.write(bitmap_data);
+                        fos.flush();
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    String value = Base64.encodeToString(bitmap_data, Base64.DEFAULT); //image to base64
+                    String value2 = Base64.encodeToString(bitmap_data2, Base64.DEFAULT); //image to base64
+                    ArrayList<String> valueList = new ArrayList<>();
+                    valueList.add(value);
+                    valueList.add(value2);
+
+                    Log.e("listfoto", String.valueOf(valueList.size()));
+
+                    RequestBody reqFile = RequestBody.create(MediaType.parse("image/*"), file);
+                    MultipartBody.Part body = MultipartBody.Part.createFormData("image", file.getName(), reqFile);
+
+                    tokenManager = TokenManager.getInstance(getSharedPreferences("prefs",
+                            Context.MODE_PRIVATE));
+                    ApiClientWelearn api = ServerWelearn.createService(ApiClientWelearn.class);
+                    Call<ResponsePredict> upload = api.predict(valueList, "9", "Bearer " + tokenManager.getToken());
+
+                    final SweetAlertDialog pDialog = new SweetAlertDialog(AngkaLv4aActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+                    pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                    pDialog.setTitleText("Loading");
+                    pDialog.setCancelable(false);
+                    pDialog.show();
+
+                    upload.enqueue(new Callback<ResponsePredict>() {
+                        @Override
+                        public void onResponse(Call<ResponsePredict> call, Response<ResponsePredict> response) {
+                            if (response.code() == 200) {
+                                Log.e("response", response.body().getMessage());
+                                pDialog.dismiss();
+                                new SweetAlertDialog(AngkaLv4aActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                                        .setTitleText(response.body().getMessage())
+                                        .setContentText("Berhasil Dikonfirmasi")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                            @Override
+                                            public void onClick(SweetAlertDialog sDialog) {
+                                                sDialog.dismissWithAnimation();
+
+                                            }
+                                        }).show();
+
+                                padjawabangka1.clear();
+                                padjawabangka2.clear();
+                                Intent intent = new Intent(AngkaLv4aActivity.this, AngkaLv4bActivity.class);
+                                intent.putExtra("id", id);
+                                startActivity(intent);
+                            } else {
+                                pDialog.dismiss();
+                                Log.e("testes", response.raw().toString());
+                                new SweetAlertDialog(AngkaLv4aActivity.this, SweetAlertDialog.WARNING_TYPE)
+                                        .setTitleText("Error")
+                                        .setContentText("Terjadi kesalahan, mohon ulangi lagi.")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                            @Override
+                                            public void onClick(SweetAlertDialog sDialog) {
+                                                sDialog.dismissWithAnimation();
+                                            }
+                                        }).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponsePredict> call, Throwable t) {
+                            Log.e("response", t.toString());
+                            pDialog.dismiss();
+                            new SweetAlertDialog(AngkaLv4aActivity.this, SweetAlertDialog.ERROR_TYPE)
+                                    .setTitleText("Hasil")
+                                    .setContentText("Internet Anda Bermasalah")
+                                    .setConfirmText("OK")
+                                    .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                        @Override
+                                        public void onClick(SweetAlertDialog sDialog) {
+                                            sDialog.dismissWithAnimation();
+                                        }
+                                    }).show();
+                        }
+                    });
+                }
+            });
+
+        }
     }
 }
